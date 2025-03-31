aa84bc8dadf80e1b080a9a30ea678d8ec1ba2dd0
marianacarhol <marianacarrilloholguin@outlook.com>
2025-03-30 18:45:14 -0700

    conexion a DB

--- index.ts
+++ index.ts
@@ -1,13 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import apiRouter from './src/routes';
+import connectionDB from './connection/connection';
 
-const app: Express = express();
 const morgan = require('morgan');
-const port = 3000;
+const app: Express = express();
+const port = 3006;
 
 app.use(morgan('dev'));
 app.use(express.json()); 
 app.use(apiRouter);
+
+connectionDB();
 
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`);
